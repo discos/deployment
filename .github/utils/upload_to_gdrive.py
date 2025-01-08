c69e5856ba5b49c46d6c86af0b7483cd49e4e736
Giuseppe Carboni <giuseppecarboni89@live.com>
2025-01-08 15:08:44 +0100

    Updated workflow

--- .github/utils/upload_to_gdrive.py
+++ .github/utils/upload_to_gdrive.py
@@ -6,7 +6,8 @@ from googleapiclient.http import MediaFileUpload
 import os
 
 TOKEN_FILE = 'token.json'
-FILE_PATH = '/home/runner/discos_manager.ova'
+VM_FILE_PATH = '/home/runner/discos_manager.ova'
+ARCHIVE_FILE_PATH = '/home/runner/vagrant.tar.gz'
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
 
 # Create the token file from the GH Secret
@@ -18,12 +19,18 @@ creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
 if creds.expired and creds.refresh_token:
     creds.refresh(Request())
 
-# Prepare the file to be uploaded
+# Prepare the files to be uploaded
 service = build('drive', 'v3', credentials=creds)
-media = MediaFileUpload(FILE_PATH, resumable=True)
+vm_media = MediaFileUpload(VM_FILE_PATH, resumable=True)
 service.files().update(
     fileId=os.environ.get('PROVISIONED_VM_GDRIVE_ID'),
-    media_body=media,
+    media_body=vm_media,
+    fields='id'
+).execute()
+archive_media = MediaFileUpload(ARCHIVE_FILE_PATH, resumable=True)
+service.files().update(
+    fileId=os.environ.get('PROVISIONED_ARCHIVE_GDRIVE_ID'),
+    media_body=archive_media,
     fields='id'
 ).execute()
 
